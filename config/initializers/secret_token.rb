6845743e7d73d7d15f2ab8e7a2d6b80f2ca2cb67
Thomas Nitsche <thomas.nitsche@gmail.com>
2012-04-15 23:19:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuckeTraeumtGerade::Application.config.secret_token = '72d21c6165cce33f142e4c30ae9c7feb289f4e6f25b76c306093bad6da3ca124319ea8b782be67bd267f46181adab8b8e7dc6cf0b3a72f1701521a9cb93a8770'
